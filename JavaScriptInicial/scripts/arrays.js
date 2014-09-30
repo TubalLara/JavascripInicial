396d017f2c29c6c9483f2a6e4cb54cc335e7540f
TubalLara <tublargar@gmail.com>
2014-09-30 14:19:15 +0200

    JavaInicial

---
+++ JavaScriptInicial/scripts/arrays.js
@@ -0,0 +1,15 @@
+﻿var array1 = new Array(4);
+
+var array2 = [];
+
+var array3 = [1, 2, 6, 8, 7];
+
+
+array2[0] = "Pepe";
+array1[0] = "Eva";
+
+
+
+for (i = 0; i < array3.length; i++) {
+    document.write(array3[i] + "<br/ >");
+}
---
+++ JavaScriptInicial/scripts/script1.js
@@ -0,0 +1,15 @@
+﻿function espar(numero1) {
+
+    return numero1 % 2 == 0;
+}
+
+
+
+
+//var numero = prompt("Dime un numero");
+
+for (var i = 0; i < 10; i++) {
+    document.writeln("El numero " + i + (espar(i) ? " es par" : " no es par"));
+    document.writeln("<br />");
+}
+
